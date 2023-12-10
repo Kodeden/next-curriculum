c90ea89d126e9daeceb5ddc0521e6dd027dcc610
Manav Misra <manavm@visionwebsoft.com>
2023-12-09 18:26:32 -0600

    feat: 💄 improve typography

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -2,6 +2,7 @@ import type { MDXComponents } from "mdx/types";
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
+    h1: ({ children }) => <h1 className="text-4xl font-bold">{children}</h1>,
     ...components,
   };
 }
